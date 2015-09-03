d5f645a81b4495a8e719abb1df053760eb7ad62e
gmg218 <gmg218@lehigh.edu>
2015-09-03 18:28:20 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+// Gabriel Gaydos
+// hw01
+//
+// class
+public class WelcomeClass {
+// main method
+    public static void main(String[ ] args) {
+// print statements
+        System.out.println("   -----------   ");
+        System.out.println("   | WELCOME |   ");
+        System.out.println("   -----------   ");
+        System.out.println("   ^ ^ ^ ^ ^ ^   ");
+        System.out.println("   / \\/ \\/ \\/ \\/ \\  ");
+        System.out.println("   <-G--M--G--2--1--8->   ");
+        System.out.println("   \\ /\\ /\\ /\\ /\\ /  ");
+        System.out.println("   v  v  v  v  v  v   ");
+        System.out.println("My family from California came to New york for the first time");
+        System.out.println("in years for my fathers funeral in early June.");
+    }
+}
