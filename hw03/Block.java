c422f1c0eb308ac4bef2807f2ff5fd6311b034da
gmg218 <gmg218@lehigh.edu>
2015-09-15 20:52:58 +0000

    updated Block

--- hw03/Block.java
+++ hw03/Block.java
@@ -13,8 +13,8 @@ public class Block {
         double w= scan.nextDouble(); // creating width and making it be the next input by the user
         System.out.print("Enter the height of the block ");
         double h= scan.nextDouble(); // creating height and making it be the next input by the user
-        double V= ((l * w) * h);
-        double SA= (((2*l*w) + 2*w*h) + 2*h*l);
+        double V= ((l * w) * h); // creating volume and calculating it with the length, width, and height
+        double SA= (((2*l*w) + 2*w*h) + 2*h*l); // creating surface area and calculating it
         System.out.println("The volume of the block with dimensions " + l + " x " + w + " x " + h + " is: " + V);
         System.out.println("The surface area of the block is: " + SA);
     }
